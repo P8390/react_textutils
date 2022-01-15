30290b69d8f2fa5e6073553999fa1946d772c691
Pankaj Kumar <pankaj@screen-magic.com>
2022-01-15 21:19:42 +0530

    [Pankaj] Issue Fixes

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -19,8 +19,9 @@ export default function TextForm(props) {
 
     const copyText = () => {
         let text = document.getElementById("mybox")
-        text.select()
+        // text.select()
         navigator.clipboard.writeText(text.value)
+        props.showAlert("Copied to Clipboard", "success")
     }
 
     const handleOnChange = (event) => {
@@ -35,17 +36,17 @@ export default function TextForm(props) {
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
             </div>
-            <button className='btn btn-primary mx-2' onClick={handleUpCase}>Convert to Upppercase</button>
-            <button className='btn btn-primary mx-2' onClick={handleLowerCase}>Convert to Lowercase</button>
-            <button className='btn btn-primary mx-2' onClick={clearText}>ClearText</button>
-            <button className='btn btn-primary mx-2' onClick={copyText}>CopyText</button>
+            <button disabled={text.length===0} className='btn btn-primary mx-2' onClick={handleUpCase}>Convert to Upppercase</button>
+            <button disabled={text.length===0} className='btn btn-primary mx-2' onClick={handleLowerCase}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className='btn btn-primary mx-2' onClick={clearText}>ClearText</button>
+            <button disabled={text.length===0} className='btn btn-primary mx-2' onClick={copyText}>CopyText</button>
         </div>
         <div className='container' my-3="true">
             <h2>Your text summary</h2>
-            <p>{text.length>0?text.split(" ").length:0} words and {text.length} characters</p>
-            <p>{text.length>0?0.3 * text.split(" ").length:0} minutes to read</p>
+            <p>{text.split(/\s+/).filter((element) =>{ return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.3 * text.split(/s+/).filter((element) =>{ return element.length!==0}).length} minutes to read</p>
             <h2>Text Preview</h2>
-            <p>{text.length>0?text:"Enter something to preview here"}</p>
+            <p>{text.length>0?text:"Nothing to preview"}</p>
         </div>
         </>
     )
